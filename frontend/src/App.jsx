66e07310a0ee7a5ea6192a053a2ef73313075e3c
AmaduKamara <amaduskamara@gmail.com>
2026-04-19 22:54:04 +0000

    Add initial frontend code for Amkam's ExT

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class='text-3xl font-bold underline text-teal-500 mt-6 text-center'>
+        Expense tracker
+      </h1>
+    </div>
+  );
+};
+
+export default App;
