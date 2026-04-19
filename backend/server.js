064c5d3994fc0c59d21435a9cfe0321e71138ea6
AmaduKamara <amaduskamara@gmail.com>
2026-04-19 23:20:14 +0000

    Create database connection method and used in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const port = 4000;
@@ -9,7 +10,9 @@ const port = 4000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // DATABASE
+connectDB();
 
 // ROUTES
 app.get("/", (req, res) => {
